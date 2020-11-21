ef3aa9915ed29c2ce769cf17bde25fa512c15b7b
cawolfand <cawolf@live.com>
2020-11-20 16:34:51 -0800

    Create pub.php

---
+++ pub.php
@@ -0,0 +1,35 @@
+<?php
+ 
+$client = new Mosquitto\Client();
+$client->onConnect('connect');
+$client->onDisconnect('disconnect');
+$client->onPublish('publish');
+$client->connect("localhost", 1883, 5);
+ 
+while (true) {
+        try{
+                $client->loop();
+                $mid = $client->publish('/mqtt', "Hello from PHP");
+                $client->loop();
+        }catch(Mosquitto\Exception $e){
+                return;
+        }
+        sleep(2);
+}
+ 
+$client->disconnect();
+unset($client);
+ 
+function connect($r) {
+        echo "I got code {$r}\n";
+}
+ 
+function publish() {
+        global $client;
+        echo "Mesage published\n";
+        $client->disconnect();
+}
+ 
+function disconnect() {
+        echo "Disconnected cleanly\n";
+}
